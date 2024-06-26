608ac1ddb63467a6264c52bbfa4a7ac5399a4e3d
himari-karesaki <himari_karesaki@schoo.jp>
2024-06-26 14:19:09 +0900

    lesson3のtask01を作成しました

---
+++ lesson03/task01/task01.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var subtotal float64 = 1500                  //商品の原価
+	taxPrice := tax()                            //消費税額
+	var taxProduct float64 = subtotal + taxPrice //商品の税込価格
+
+	fmt.Printf("%v円の商品の税込価格は%v円(消費税は%v円)です\n", subtotal, taxProduct, taxPrice)
+}
+
+// 消費税を計算する関数
+func tax() float64 {
+	var price float64 = 1500  //商品の原価
+	var taxRate float64 = 0.1 //消費税率
+	return price * taxRate
+}
