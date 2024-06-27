57d106e6713f589e9cf702066b24c52528c139e9
himari-karesaki <himari_karesaki@schoo.jp>
2024-06-27 14:48:59 +0900

    task02を作成しました

---
+++ lesson04/task02/task02.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//英単語を入力する変数の宣言
+	var word string
+	//英単語を格納するスライスの宣言
+	words := make([]string, 0)
+	//アルファベットの文字数を格納する変数の宣言
+	counts := make(map[rune]int)
+
+	fmt.Print("英単語を入力してください(endと入力するとループを終了します): ")
+}
