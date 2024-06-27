f8bd8e9038a9b3c03e27884405ad77fa45e89e71
himari-karesaki <himari_karesaki@schoo.jp>
2024-06-27 16:28:51 +0900

    入力した英単語が表示されるように修正しました

--- lesson04/task02/task02.go
+++ lesson04/task02/task02.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	//英単語を入力する変数の宣言
@@ -8,7 +10,9 @@ func main() {
 	//英単語を格納するスライスの宣言
 	words := make([]string, 0)
 	//アルファベットの文字数を格納する変数の宣言
-	counts := make(map[rune]int)
+	// counts := make(map[rune]int)
+	// counts['a']++
+	// fmt.Println(counts)
 
 	fmt.Print("英単語を入力してください(endと入力するとループを終了します): ")
 
@@ -16,8 +20,18 @@ func main() {
 	for {
 		//英単語の標準入力
 		fmt.Scan(&word)
+
+		// //mapに文字数を格納する
+		// counts := make(map[rune]int)
+		// fmt.Println(counts)
+
 		//endが入力されたらループを終了
 		if word == "end" {
+			//入力した英単語の表示
+			fmt.Println("入力した英単語：")
+			for _, word := range words {
+				fmt.Println(word)
+			}
 			break
 		}
 
@@ -25,15 +39,25 @@ func main() {
 		words = append(words, word)
 		fmt.Println(words)
 
-		
 	}
+
+	// マップの要素aをカウント
+	//もし文字aが入っていたらカウントする
+	// var a string
+	// if value, exists := counts["a"]; exists {
+	// 	counts["a"]++
+	// 	fmt.Println("a:", value)
+	// } else {
+	// 	fmt.Println("a does not exist")
+	// }
+
 }
 
-//アルファベットの文字数をカウントする関数
-func countA(a []string) int {
-	for _, a := range word {
-	counts["a"]++
-}
+// //アルファベットの文字数をカウントする関数
+// func countA(a []string) int {
+// 	for _, a := range word {
+// 	counts["a"]++
+// }
 
 // func countWords(words []string) int {
 // 	// キーの存在チェック
