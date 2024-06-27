8d3a98d607b27a8eb3e155d48a6649b503893372
himari-karesaki <himari_karesaki@schoo.jp>
2024-06-27 14:17:01 +0900

    修正しました

--- lesson04/task01/task01.go
+++ lesson04/task01/task01.go
@@ -1,59 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	//点数を入力する変数
+	//点数を入力する変数(intは符号付き整数)
 	var score int
 
 	//点数を格納するスライスを作成
-	scores := make([]int, 5, 10)
+	//容量の最大値はあえて指定しない
+	scores := make([]int, 0)
 
 	//無限ループの設定
 	for {
-		//繰り返し処理
 		//「点数を入力してください：」を表示させる
 		fmt.Println("点数を入力してください：")
 		//点数の入力を受け付ける
 		fmt.Scan(&score)
+
 		//「-1」が入力されたら無限ループを抜ける
 		if score == -1 {
+			if length := len(scores); length == 0 {
+				//点数が入力されなかった場合の処理
+				fmt.Println("点数が入力されませんでした")
+			}
 			break
 		}
+
 		//入力した点数をスライスに追加
 		scores = append(scores, score)
+		fmt.Println(scores)
 
+		//点数の合計値を求める
+		var sum float64 = sumScore(scores)
+
+		//人数を求める処理
+		count := len(scores)
+		// fmt.Println(count)
+
+		// 平均点を求める処理
+		average := sum / float64(count)
+		// fmt.Println(average)
+
+		var newAverage float64 = float64(average)
+
+		//averageを小数第一位まで四捨五入する
+		Round(average, 1)
+		// fmt.Println(newAverage)
+
+		// 「○○人のテストの平均点は△△点です」と表示させる
+		// 平均点の出力の際は小数点第一位まで表示させる
+		fmt.Printf("%v人のテストの平均点は%.1f点です\n", count, newAverage)
 	}
 
-	//点数の合計値を求める
-	for index, value := range scores {
-		//繰り返し値が足される処理を行う
-	}
-	//人数を求める
-	count := len(scores)
-
-	// 平均点を求める
-	average := 点数の合計値 / count
-
-	float64(average)
-
-	//averageを小数第一位まで四捨五入する関数
-	func Round(num float64, pos int) float64 {
-		shift := math.Pow10(pos)             // 小数の位置をずらすためのシフト値を算出
-		shiftedNum := num * shift            // 四捨五入したい桁を小数第一位にずらす
-		roundedNum := math.Round(shiftedNum) // 小数第一位を四捨五入する
-		result := roundedNum / shift         // 小数の位置を元に戻す
-		return result
-	}
-	//averageを小数第一位まで四捨五入する
-	Round(average, 1)
-
-	//「○○人のテストの平均点は△△点です」と表示させる
-	//平均点の出力の際は小数点第一位まで表示させる
-	fmt.Printf("%.1f人のテストの平均点は%v点です", count, average)
-
-if length := len(scores);length == 0 {
-	//点数が入力されなかった場合の処理
-	fmt.Println("点数が入力されませんでした")
 }
+
+// 点数の合計値を求める関数
+func sumScore(numbers []int) float64 {
+	sum := 0
+	// var number int
+	for _, number := range numbers {
+		sum += number
+	}
+	return float64(sum)
+}
+
+// averageを小数第一位まで四捨五入する関数
+func Round(num float64, pos int) float64 {
+	shift := math.Pow10(pos)             // 小数の位置をずらすためのシフト値を算出
+	shiftedNum := num * shift            // 四捨五入したい桁を小数第一位にずらす
+	roundedNum := math.Round(shiftedNum) // 小数第一位を四捨五入する
+	result := roundedNum / shift         // 小数の位置を元に戻す
+	return result
 }
