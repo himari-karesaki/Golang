b48fa9145423d41df0ab62f9df04e62d1a9aa100
himari-karesaki <himari_karesaki@schoo.jp>
2024-06-27 11:23:21 +0900

    task01を作成しました

---
+++ lesson04/task01/task01.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	//点数を入力する変数
+	score := 
+
+	//点数を格納するスライス
+	scores := []int{}
+
+	//無限ループの設定
+	for {
+		//繰り返し処理
+	}
+
+    // //点数をスライスに追加
+    // scores = append(scores, score)
+
+    // //スライスの長さを出力
+    // fmt.Println(len(scores))
+
+    // //スライスの最後の要素を出力
+    // fmt.Println(scores[len(scores)-1])
+
+    // //スライスの最後の要素を削除
+    // scores = scores[:len(scores)-1]
+
+    // //スライスの長さを出力
+    // fmt.Println(len(scores))
+
+    // //スライスの最後の要素を出力
+    // fmt.Println(scores[len(scores)-1])
+
+    // //スライスの最後の要素を削除
+    // scores = scores[:len(scores)-1]
+
+    // //スライスの長さを出力
+}
