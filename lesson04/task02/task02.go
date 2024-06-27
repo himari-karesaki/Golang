4c6b7d64a60dd00778451631e4454a65dd54736a
himari-karesaki <himari_karesaki@schoo.jp>
2024-06-27 15:21:23 +0900

    task02を作成しました

--- lesson04/task02/task02.go
+++ lesson04/task02/task02.go
@@ -11,4 +11,33 @@ func main() {
 	counts := make(map[rune]int)
 
 	fmt.Print("英単語を入力してください(endと入力するとループを終了します): ")
+
+	//無限ループの作成
+	for {
+		//英単語の標準入力
+		fmt.Scan(&word)
+		//endが入力されたらループを終了
+		if word == "end" {
+			break
+		}
+
+		//入力した値をスライスwordsに格納
+		words = append(words, word)
+		fmt.Println(words)
+
+		
+	}
 }
+
+//アルファベットの文字数をカウントする関数
+func countA(a []string) int {
+	for _, a := range word {
+	counts["a"]++
+}
+
+// func countWords(words []string) int {
+// 	// キーの存在チェック
+// 	if value, exists := m["a"]; exists {
+// 		fmt.Println("a:", value)
+// 	}
+// }
