f29d0252369b943aba078c4ca80c7ec7e60b7a93
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-03 15:47:52 +0900

    修正しました

--- lesson08/task01/task01.go
+++ lesson08/task01/task01.go
@@ -3,88 +3,58 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"unicode/utf8"
+	"sync"
 )
 
-// file1:="./file1.txt"
-// file2:="./file2.txt"
-// file3:="./file3.txt"
+// ファイルの読み込みを行う関数
+func readFile(file []string, ch chan<- int, wg *sync.WaitGroup) {
 
-func readFile() {
-	//引数で指定したファイルを読み込む処理
-	//file1の読み込み
-	// ReadFile関数
-	file1, err := os.ReadFile("file1.txt")
-	if err != nil {
-		fmt.Println("ファイルを読み込めませんでした:", err)
-		return
-	}
-	fmt.Println(utf8.RuneCountInString(string(file1)))
+	//file文字数をintにしたいので、int型のスライスを作成
+	sum := 0
 
-	//file2の読み込み
-	file2, err := os.ReadFile("file2.txt")
-	if err != nil {
-		fmt.Println("ファイルを読み込めませんでした:", err)
-		return
-	}
-	fmt.Println(utf8.RuneCountInString(string(file2)))
+	// // 文字数のカウントをチャネルに送信
+	// ch <- sum
+	//文字数を格納する変数
+	//ファイルを読み込んで文字数をカウントする
+	for i, _ := range file {
+		data, err := os.ReadFile(file[i])
+		if err != nil {
+			fmt.Println("ファイルを読み込めませんでした:", err)
+			return
+		}
+		// 読み込んだデータの文字数をカウント
+		// wg.Done()
+		sum = len(data)
 
-	//file3の読み込み
-	file3, err := os.ReadFile("file3.txt")
-	if err != nil {
-		fmt.Println("ファイルを読み込めませんでした:", err)
-		return
 	}
-	fmt.Println(utf8.RuneCountInString(string(file3)))
-	//内容の文字数を計算する処理
-	// count := 0
-	//文字数のカウントのスライス
-	//content1{a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}
-	// for _, word1 := range content1 {
-	// 	int(word1)
-	// 	count(word1)++
-	// 	return word1
-	// }
-	// for _, word2 := range content2 {
-	// 	count++
-	// 	return string(word2)
-	// }
-	//結果をチャネル経由でmain関数に送信する処理
+
+	//文字数のカウントをチャネルに送信
+	ch <- sum
+	wg.Done()
 }
 
 func main() {
-	//スライスの作成
-	//"file1.txt", "file2.txt", "file3.txt" が要素
+	//チャネルに送信された値のスライスの作成
+	ch := make(chan int)
+	//"file1.txt", "file2.txt", "file3.txt" が要素のスライスを作成
+	files := []string{"./file1.txt", "./file2.txt", "./file3.txt"}
 
-	//goroutineを立ち上げてreadFile関数を実行する
-	readFile()
-	//結果を受信するチャネルを作成
-	// ch := make(chan int)
+	// WaitGroupを作成
+	var wg sync.WaitGroup
+	//readFile関数を実行する
+	for i := 0; i < len(files); i++ {
+		//goroutineを立ち上げて
+		wg.Add(1)
+		go readFile(files, ch, &wg)
+		fmt.Println(files[i])
+	}
+	fmt.Println("ゴルーチンの完了を待っています...")
 
-	//
-	// go readFile(content1, ch)
-	// go readFile(context2, ch)
-	// readFile()
-	//file1
-	//file2
-	//file3
+	wg.Wait() // すべてのゴルーチンの完了を待つ
 
-	//main関数からチャネル��由で受信する
-	//結果を表示する
+	// チャネルから結果を受信
+	sum := <-ch
 
-	//waitGroupを使って全てのgoroutineが終了するのを待つ
-
-	//file1のGoroutineから文字数を受け取る
-	//file2のGoroutineから文字数を受け取る
-	//file3のGoroutineから文字数を受け取る
-
-	// for _, v := range files {
-	// 	//合計を計算する処理
-	// 	count++
-
-	// 	//最終的な文字数を表示する
-	// 	fmt.Println(count)
-	// }
-
+	fmt.Println("Total sum:", sum)
+	fmt.Println("すべてのゴルーチンが終了しました")
 }
