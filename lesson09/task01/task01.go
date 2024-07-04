075f4f8026d123195b3b21732dc1590f408c81d9
himari-karesaki <himari_karesaki@schoo.jp>
2024-07-04 16:15:55 +0900

    task01を修正しました

--- lesson09/task01/task01.go
+++ lesson09/task01/task01.go
@@ -18,7 +18,8 @@ type Book struct {
 func main() {
 	var title string
 
-	fmt.Scanf("検索するタイトルを入力してください: %s", title)
+	fmt.Print("検索するタイトルを入力してください: ")
+	fmt.Scan(&title)
 
 	// データベースに接続
 	db, err := sql.Open("mysql", "root:_MySQLPassword1@tcp(localhost:3306)/bookstore?charset=utf8mb4&parseTime=true&loc=Local")
@@ -33,10 +34,10 @@ func main() {
 
 	var book Book
 
-	err = db.QueryRow("SELECT * FROM books WHERE id = ?", dbTitle).Scan(&book.ID, &book.Title, &book.Price, &book.CreatedAt)
+	err = db.QueryRow("SELECT * FROM books WHERE title = ?", dbTitle).Scan(&book.ID, &book.Title, &book.Price, &book.CreatedAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			fmt.Println("指定されたIDのレコードが見つかりませんでした")
+			fmt.Println("指定されたタイトルのレコードが見つかりませんでした")
 		} else {
 			fmt.Println("クエリの実行に失敗しました:", err)
 		}
